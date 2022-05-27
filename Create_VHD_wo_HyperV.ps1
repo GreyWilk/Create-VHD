8bfe2c22d389a367d5094e831d0be1505177cff3
Mr Grey Wilk <41646439+GreyWilk@users.noreply.github.com>
2022-05-27 13:21:31 +0200

    File naming and logging
    Fixed unique file names for looping the script. Added better logging.

--- Create_VHD_wo_HyperV.ps1
+++ Create_VHD_wo_HyperV.ps1
@@ -1,7 +1,8 @@
 [CmdletBinding(SupportsShouldProcess)]
 param (
     [int][validateset("2","5","10","50","100","250","500")]$VHDSizeInGB,
-    [bool]$AttachVHDToLocalComputer = $false
+    [bool]$AttachVHDToLocalComputer = $false,
+    [switch]$RunCleanup
 )
 
 ####
@@ -11,58 +12,120 @@ param (
 # 
 ####
 
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+function Write-timeLog {
+    param(
+        [parameter(Mandatory=$true)]$logText,
+        [parameter(Mandatory=$false)][validateSet('Cyan','Yellow','Red','Green','Gray')]$logColor,
+        [parameter(Mandatory=$false)][validateSet('Info','Warning','Error','Debug')]$logType
+    )
+    $logOutput = $null
+    $logOutput += "[$(Get-Date -Format 'HH:mm:ss')] "
 
-
-If ( -not $(Get-Item C:\windows\system32\diskpart.exe -ErrorAction SilentlyContinue) ){
-    Throw "Unable to locate DISKPART @ location c:\windows\system32\ !"
-}
-If ( -not $($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) ){
-    Throw "Script not running as Admin!"
-}
-
-$UsableVolumes = Get-Volume | Where-Object { $_.DriveLetter -ne $null -and $_.DriveType -eq "Fixed" }
-$MostFreeSpaceVolume = $UsableVolumes | Sort-Object SizeRemaining -Descending | Select-Object -First 1
-$Date = (Get-Date -Format 'yyyy-MM-dd')
-
-## Create diskpart tasks
-$DiskPartCommandsFileName = "DISKPART_CMD_$(get-date -format 'yyyy-MM-dd').txt"
-
-if( $AttachVHDToLocalComputer -eq $false ) {
-$DiskPartCommands = @"
-create vdisk file=$($MostFreeSpaceVolume.DriveLetter):\VHD_$Date.vhd maximum=$($VHDSizeInGB*1024) type=expandable
-"@
-} else {
-$DiskPartCommands = @"
-create vdisk file=$($MostFreeSpaceVolume.DriveLetter):\VHD_$Date.vhd maximum=$($VHDSizeInGB*1024) type=expandable
-select vdisk file=$($MostFreeSpaceVolume.DriveLetter):\VHD_$Date.vhd
-attach vdisk
-"@
-}
-
-$DiskPartCommands | Out-File "$($MostFreeSpaceVolume.DriveLetter):\$DiskPartCommandsFileName" -Encoding ascii
-
-## Execute DISKPART commands
-DISKPART /s "$($MostFreeSpaceVolume.DriveLetter):\$DiskPartCommandsFileName" >> "$($MostFreeSpaceVolume.DriveLetter):\VHD_$Date.log"
-
-if( $AttachVHDToLocalComputer -eq $true ) {
-    Start-Sleep 2
-    $DiskInfo = (Get-Disk | Where-Object { $_.Location -eq "$($MostFreeSpaceVolume.DriveLetter):\VHD_$Date.vhd" })
-    Write-Host "UniqueID of disk is: $([string]$DiskInfo.UniqueId)"
-    $InitializedDisk = Initialize-Disk -UniqueId $([string]$DiskInfo.UniqueId) -PassThru
-
-    ## Find first unused drive letter
-    $DriveLetterToUse = $null
-    $UsedDriveLetters = ((Get-Volume | Select-Object DriveLetter).DriveLetter)
-    68..90 | ForEach-Object {
-        If ($UsedDriveLetters -notcontains [char]$PSItem) {
-            $DriveLetterToUse = [char]$PSItem
-            break
+    if( -not $logColor -and -not $logType ){
+        $logColor = "Cyan"
+        $logTypeString = "[INFO] "
+    } elseif( -not $logColor -and $logType ){
+        switch( $logType ){
+            'Info'      { $logTypeString = "[INFO] "; $logColor = "Cyan" }
+            'Warning'   { $logTypeString = "[WRNG] "; $logColor = "Yellow" }
+            'Error'     { $logTypeString = "[ERR]  "; $logColor = "Red" }
+            'Debug'     { $logTypeString = "[DBG]  "; $logColor = "Gray" }
+            default     { $logTypeString = "[INFO] "; $logColor = "White" }
+        }
+    } else {
+        switch( $logType ){
+            'Info'      { $logTypeString = "[INFO] " }
+            'Warning'   { $logTypeString = "[WRNG] " }
+            'Error'     { $logTypeString = "[ERR]  " }
+            'Debug'     { $logTypeString = "[DBG]  " }
+            default     { $logTypeString = "[INFO] " }
         }
     }
-    
-    ## Create new partition and format VHD to NTFS
-    $NewPartition = New-Partition -InputObject $InitializedDisk -UseMaximumSize -DriveLetter $DriveLetterToUse
-    Format-Volume -DriveLetter $($NewPartition.DriveLetter) | Out-Null
-    Set-Volume -DriveLetter $($NewPartition.DriveLetter) -NewFileSystemLabel "vDisk_$DriveLetterToUse"
+
+    $logOutput += $logTypeString
+    $logOutput += $logText
+    Write-Host -ForegroundColor $logColor -Object $logOutput
 }
+
+try{
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+
+    If ( -not $(Get-Item C:\windows\system32\diskpart.exe -ErrorAction SilentlyContinue) ){
+        Throw "Unable to locate DISKPART @ location c:\windows\system32\ !"
+    }
+    If ( -not $($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) ){
+        Throw "Script not running as Admin!"
+    }
+
+    $UsableVolumes = Get-Volume | Where-Object { $_.DriveLetter -ne $null -and $_.DriveType -eq "Fixed" }
+    $MostFreeSpaceVolume = $UsableVolumes | Sort-Object SizeRemaining -Descending | Select-Object -First 1
+    $Date = (Get-Date -Format 'yyyy-MM-dd')
+    $GUID = (New-Guid).Guid.Substring(0,8)
+    $VHDName = ("VHD_" + $Date + "_" + $GUID)
+
+    ## Create diskpart tasks
+    $DiskPartCommandsFileName = "DISKPART_CMD_$(get-date -format 'yyyy-MM-dd')_$GUID.txt"
+    $DiskPartCommandsFile = New-Item -ItemType File -Path "$($MostFreeSpaceVolume.DriveLetter):\" -Name $DiskPartCommandsFileName
+
+    if( $AttachVHDToLocalComputer -eq $false ) {
+        Write-timeLog  -logType Debug -logText "Creating run file for DISKPART (no attach)."
+        "create vdisk file=$($MostFreeSpaceVolume.DriveLetter):\$VHDName.vhd maximum=$($VHDSizeInGB*1024) type=expandable" | Out-File -FilePath $DiskPartCommandsFile.FullName -Encoding ascii -Append
+    } else {
+        Write-timeLog  -logType Debug -logText "Creating run file for DISKPART (with attach)."
+        "create vdisk file=$($MostFreeSpaceVolume.DriveLetter):\$VHDName.vhd maximum=$($VHDSizeInGB*1024) type=expandable" | Out-File -FilePath $DiskPartCommandsFile.FullName -Encoding ascii -Append
+        "select vdisk file=$($MostFreeSpaceVolume.DriveLetter):\$VHDName.vhd" | Out-File -FilePath $DiskPartCommandsFile.FullName -Encoding ascii -Append
+        "attach vdisk" | Out-File -FilePath $DiskPartCommandsFile.FullName -Encoding ascii -Append
+    }
+
+    #$DiskPartCommands | Out-File "$($MostFreeSpaceVolume.DriveLetter):\$DiskPartCommandsFileName" -Encoding ascii
+
+    ## Execute DISKPART commands
+    Write-timeLog  -logType Debug -logText "Running DISKPART."
+    DISKPART /s "$($MostFreeSpaceVolume.DriveLetter):\$DiskPartCommandsFileName" >> "$($MostFreeSpaceVolume.DriveLetter):\$VHDName.log"
+
+    if( $AttachVHDToLocalComputer -eq $true ) {
+        $DiskInfo = (Get-Disk | Where-Object { $_.Location -eq "$($MostFreeSpaceVolume.DriveLetter):\$VHDName.vhd" })
+        Write-timeLog -logType Debug -logText "UniqueID of disk is: $([string]$DiskInfo.UniqueId)"
+        Write-timeLog -logType Debug -logText "Initializing disk..."
+        $InitializedDisk = Initialize-Disk -UniqueId $([string]$DiskInfo.UniqueId) -PassThru
+        If( $InitializedDisk ) {
+            Write-timeLog -logType Debug -logText "Initializing disk completed."
+
+            ## Find first unused drive letter
+            $DriveLetterToUse = $null
+            $UsedDriveLetters = ((Get-Volume | Select-Object DriveLetter).DriveLetter)
+            68..90 | ForEach-Object {
+                If ($UsedDriveLetters -notcontains [char]$PSItem -and $DriveLetterToUse -eq $null) {
+                    $DriveLetterToUse = [char]$PSItem
+                }
+            }
+            
+            ## Create new partition and format VHD to NTFS
+            Write-timeLog -logType Debug -logText "Creating partition."
+            $NewPartition = New-Partition -InputObject $InitializedDisk -UseMaximumSize -DriveLetter $DriveLetterToUse
+            Format-Volume -DriveLetter $($NewPartition.DriveLetter) | Out-Null
+            Set-Volume -DriveLetter $($NewPartition.DriveLetter) -NewFileSystemLabel "vDisk_$DriveLetterToUse"
+
+        } else {
+            Write-timeLog -logType Debug -logText "Failed to initalize disk"
+        }
+
+        
+    }
+
+    if( $RunCleanup ) {
+        Write-timeLog -logType Debug -logText "Running cleanup!"
+        Write-timeLog -logType Debug -logText "Removing DISKPART command file."
+        Remove-Item -Path "$($MostFreeSpaceVolume.DriveLetter):\$DiskPartCommandsFileName"
+        Write-timeLog -logType Debug -logText "Removing VHD create log."
+        Remove-Item -Path "$($MostFreeSpaceVolume.DriveLetter):\$VHDName.log"
+    }
+} catch {
+    Write-timeLog -logType Error -logText "Error creating VHD."
+    Write-timeLog -logType Error -logText  $error[0].Exception
+    Write-timeLog -logType Error -logText  $error[0].InvocationInfo.PositionMessage
+    $Error.remove($Error[0])
+    }
+## Create huge file on vdisk for fun
+# [io.file]::Create("$($DriveLetterToUse):\bigblob.txt").SetLength((Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$($DriveLetterToUse):'").FreeSpace - 200MB).Close
